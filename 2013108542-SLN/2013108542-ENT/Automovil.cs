bfaa9ace1a6b7b4d09cf731a8e04cd01aca5ad68
ITALO VASQUEZ <italo_vasquez@usmp.pe>
2017-05-13 04:24:57 -0500

    Prueba 2

---
+++ 2013108542-SLN/2013108542-ENT/Automovil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2013108542_ENT
+{
+    class Automovil
+    {
+    }
+}
