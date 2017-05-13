f2a8e77a67e21b5d3854495b708e6051b11bb99f
ITALO VASQUEZ <italo_vasquez@usmp.pe>
2017-05-13 06:17:57 -0500

    Prueba 4

--- 2013108542-SLN/2013108542-CON/Program.cs
+++ 2013108542-SLN/2013108542-CON/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using _2013108542_ENT;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _2013108542_CON
@@ -11,7 +13,7 @@ namespace _2013108542_CON
         
             static void Main(string[] args)
             {
-                var ensambladora = new Ensambladora.Entities.Ensambladora();
+                var ensambladora = new _2013108542_ENT.Ensambladora();
 
                 var auto1 = ensambladora.EnsamblarCarro(TipoCarro.Automovil, TipoAuto.Sedan, TipoBus.NoDefinido);
                 var auto2 = ensambladora.EnsamblarCarro(TipoCarro.Automovil, TipoAuto.Coupe, TipoBus.NoDefinido);
